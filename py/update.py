e9af55ea56b91ec768a4a219d527ef978f30fe04
Tom Renner <tommrenner@gmail.com>
2017-04-20 23:53:22 +0100

    Move file manipulation out of add_score.

--- py/update.py
+++ py/update.py
@@ -5,27 +5,30 @@ def update():
     new_score = 'newscore.ascii'
     total_score = 'SCORE.txt'   # NB: Not the same format as SCORE.ascii
     all_records = 'RECORDS.ascii'
-    
-    new_total = add_score(new_score, total_score)
+    headers = ['Oli', 'Tom']
+    totals = pd.read_csv(total_score, names=headers)
+    score = pd.read_csv(new_score, names=headers)
 
+    
+    new_total = add_score(score, totals)
+    # Do all records and calculations on dataframe
+
+    # Save the new total
     new_total.to_csv(total_score, header=False, index=False)
     # Delete the added score
     df = pd.DataFrame().to_csv(new_score, header=False, index=False)
 
-def add_score(in_file, out_file):
-    headers = ['Oli', 'Tom']
-    totals = pd.read_csv(out_file, names=headers)
-    score = pd.read_csv(in_file, names=headers)
-
+def add_score(score, totals):
     total = totals.tail(1)
     total_idx = total.index.values[0]+1
     score_len = score.tail(1).index.values[0]+1
     end_idx = total_idx+score_len
 
-    df = pd.DataFrame(columns=headers)
+    df = pd.DataFrame(columns=['Oli','Tom'])
     for x in range(0, score_len):
        df.loc[x] = total.values[0]
 
     return totals.append(score+df, ignore_index=True)
 
-update()
+if __name__ == '__main__':
+    update()
