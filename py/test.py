67dabde3c237828bd64d175324107294dd00236d
Tom Renner <tommrenner@gmail.com>
2017-05-12 19:50:32 +0100

    WIP commit of storing "vitals" records.

--- py/test.py
+++ py/test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import unittest
 import update
@@ -11,11 +12,20 @@ class TestRummy(unittest.TestCase):
             })
         self.new_score = pd.DataFrame({
             'Oli': [25,55,155,145],
-            'Tom': [75,145,355,380]
+            'Tom': [75,150,355,380]
             })
+
+    def assertArrayEquals(self, a, b):
+        #TODO: Print out the values of the arrays on failure
+        try:
+            for i in range(0, a.size):
+                self.assertEqual(a[0][i], b[i])
+        except:
+            raise
 
     def test_add_score(self):
         totals = update.add_score(self.new_score, self.total_score)
+        #TODO change to use assertArrayEquals
         self.assertEqual(totals.tail(1).values[0][0], 305)
         self.assertEqual(totals.tail(1).values[0][1], 520)
         self.assertEqual(totals.head(1).values[0][0], 0)
@@ -24,13 +34,28 @@ class TestRummy(unittest.TestCase):
 
     def test_add_hands(self):
         totals = update.add_score(self.new_score, self.total_score)
-        with_hands = update.add_hands(totals)
+        update.add_hands(totals)
         self.assertEqual(totals.head(1).values[0][0], 0)
         self.assertEqual(totals.tail(1).values[0][1], 520)
         self.assertEqual(totals.head(1).values[0][3], 110)
         self.assertEqual(totals.tail(1).values[0][2], -10)
         self.assertEqual(totals.tail(1).index.values[0], 7)
 
+    def test_vitals(self):
+        totals = update.add_score(self.new_score, self.total_score)
+        update.add_hands(totals)
+        r = {}
+        update.vitals(totals, r)
+        self.assertEqual(r['Total hands played'], 8)
+        self.assertEqual(r["Oli's current lead"], -215)
+        self.assertArrayEquals(r["Total scores"], [305, 520])
+        self.assertArrayEquals(r["Modes"], [25, 75])
+        self.assertArrayEquals(r["Mean scores"], [38.125, 65])
+        self.assertArrayEquals(r["Maxes"], [100, 205])
+        print(r["Maxes"])
+        #self.assertArrayEquals(r["Mins"], [-10, -20])
+        self.assertArrayEquals(r["Max swing"], [55, 110])
+        self.assertArrayEquals(np.array(r["Max lead"]), [20, 215])
 
 if __name__ == '__main__':
     unittest.main()
--- py/update.py
+++ py/update.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import csv
 
 def update():
     new_score = 'newscore.ascii'
@@ -19,16 +20,17 @@ def update():
     # Delete the added score
     df = pd.DataFrame().to_csv(new_score, header=False, index=False)
 
+
 def add_score(score, totals):
     total = totals.tail(1)
     total_idx = total.index.values[0]+1
     score_len = score.tail(1).index.values[0]+1
-
     df = pd.DataFrame(columns=['Oli','Tom'])
     for x in range(0, score_len):
        df.loc[x] = total.values[0]
 
     return totals.append(score+df, ignore_index=True)
+
 
 def add_hands(totals):
     hands = pd.DataFrame(columns=['oHands', 'tHands'])
@@ -37,6 +39,25 @@ def add_hands(totals):
     # Diff of first row is NaN - set manually
     totals['oHands'].loc[0] = totals['Oli'].loc[0]
     totals['tHands'].loc[0] = totals['Tom'].loc[0]
+
+
+def vitals(totals, records):
+    swing = totals['oHands']-totals['tHands']
+    score_difference = totals['Oli']-totals['Tom']
+    score = totals.filter(items=['Oli', 'Tom'])
+
+    records['Total hands played'] = totals.tail(1).index.values[0]+1
+    records["Oli's current lead"] = score_difference.tail(1).values[0]
+    records['Total scores'] = score.tail(1).values
+    records['Modes'] = score.mode().values
+    records['Mean scores'] = score.mean().values
+    records['Maxes'] = score.max().values
+    print(score)
+    print(score.max().values)
+    print(score.min().values)
+    records['Mins'] = score.min().values
+    records['Max swing'] = swing.max()
+    records['Max lead'] = [score_difference.max(), score_difference.min()*-1]
 
 
 if __name__ == '__main__':
